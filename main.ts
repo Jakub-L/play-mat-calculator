a14b96a588c91a4fa5d5b728e0629bc67a5cdb52
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2024-10-24 23:37:48 +0100

    Add comments

--- main.ts
+++ main.ts
@@ -2,7 +2,12 @@ const colors: string[] = ["O", "Y", "B", "G"]
 const results: Set<string> = new Set()
 let count = 0
 
-
+/**
+ * Recursively generates the tile arrangements.
+ * @param {number[]} current - The current investigated tiles and their colours.
+ * @param {number[]} colorsUsed - How many tiles of a given colour (assuming colours are just numbered
+ *    by their index) have been used so far.
+ */
 const generate = (current: number[], colorsUsed: number[]) => {
   if (current.length === 16) {
     count += 1
@@ -10,6 +15,7 @@ const generate = (current: number[], colorsUsed: number[]) => {
   else {
     for (let i = 0; i < colorsUsed.length; i++) {
       if (colorsUsed[i] < 4) {
+        // This way turns out to be much quicker than spread syntax
         // Create a new arrangement
         colorsUsed[i] += 1
         current.push(i)
